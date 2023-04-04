42b34e359fbadc726dbfa58c646704d51a9ddc1e
Giftagbro <agbro.oghenekevwe@gmail.com>
2023-04-04 14:23:01 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,28 @@
+/*
+* File:0-print_listint.c
+*Auth:Agbro gift
+*/
+
+
+
 #include "lists.h"
+
 /**
  * print_listint - this function will print elements of a linked list
  * @h: linked list of types listint_t to print
  * Return: number of nodes
  */
 
+
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *node = h;
-	size_t numbz = 0;
+	size_t nodes = 0;
 
-	while (node)
+	while (h != NULL)
 	{
-	printf("%d\n", node->n);
-	numbz++;
-	node = node->next;
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
 	}
-	return (numbz);
+	return (nodes);
 }
