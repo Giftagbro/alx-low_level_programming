bffcf4bd7295fca4d417e1511bf2c1cd73c1a1f5
Giftagbro <agbro.oghenekevwe@gmail.com>
2023-04-10 16:12:26 -0700

    0-read update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,29 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints to POSIX stdout
- * @filename: name of the file that's read
- * @letters: the number of letters read and printed
- *
- * Return: the actual  number of letters read and printed, 0 if file cannot be
- * opened or read, 0 if @filename is NULL, 0 if write fails or doesn't write
- * the expected amount of bytes
- * returns (wrt)
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: wr- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	int rd, fd, wrt;
-
-	if (filename == NULL)
-		return (0);
-	buff = malloc(letters);
-	if (buff == NULL)
-		return (0);
+	char *buf;
+	ssize_t fd;
+	ssize_t wr;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buff);
 		return (0);
-	}
-	rd = read(fd, buff, letters);
-	if (rd == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-	wrt = write(STDOUT_FILENO, buff, rd);
-	if (wrt == -1)
-	{
-		free(buff);
-		close(fd);
-		return (0);
-	}
-	/*close fd*/
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
 	close(fd);
-	free(buff);
-	return (wrt);
+	return (wr);
 }
+
